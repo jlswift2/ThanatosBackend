6d5adbc623ca168bcca85f899224cca92289aa3e
Connor McNeely <github@gilden00.com>
2023-09-27 19:30:58 -0500

    Implement Page - Update Request Handler

--- src/Thanatos.API.Infrastructure/CQRS/Pages/UpdatePageRequestHandler.cs
+++ src/Thanatos.API.Infrastructure/CQRS/Pages/UpdatePageRequestHandler.cs
@@ -1,19 +1,40 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Thanatos.API.Infrastructure.Exceptions;
 
 namespace Thanatos.API.Infrastructure.CQRS.Pages
 {
     public class UpdatePageRequestHandler : IRequestHandler<UpdatePageRequest, UpdatePageRequestResponse>
     {
-        public UpdatePageRequestHandler() { throw new NotImplementedException(); }
+        private readonly ThanatosDbContext _dbContext;
+        private readonly IMapper _mapper;
 
-        public Task<UpdatePageRequestResponse> Handle(UpdatePageRequest request, CancellationToken cancellationToken)
+        public UpdatePageRequestHandler(IMapper mapper, ThanatosDbContext dbContext)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _dbContext = dbContext;
+        }
+
+        public async Task<UpdatePageRequestResponse> Handle(UpdatePageRequest request, CancellationToken cancellationToken)
+        {
+            var page = _dbContext.Pages.FirstOrDefault(x => x.Id == request.Route.Id);
+
+            if (page == null)
+            {
+                throw new NotFoundException();
+            }
+
+            _mapper.Map(request.Body, page);
+
+            _dbContext.Pages.Update(page);
+            await _dbContext.SaveChangesAsync();
+
+            return _mapper.Map<UpdatePageRequestResponse>(page);
         }
     }
 }
